b0e4e783f4f0b9dd28b68d9dce6d85746c1fbe72
Afifsuryaramadhan1 <afifsuryaramadhan@gmail.com>
2021-12-27 04:27:19 +0700

    ready

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Periode;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UnivSeeder::class);
         $this->call(DivisiSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(PeriodeSeeder::class);
     }
 }
---
+++ database/seeders/PeriodeSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Periode;
+
+class PeriodeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Periode::insert([
+            ['periode' => 'Pembina', 'status' => 1],
+            ['periode' => '2021/2022', 'status' => 1],
+        ]);
+    }
+}
