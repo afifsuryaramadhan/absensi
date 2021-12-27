57bd5330766f349057b38f29417051554d7f9390
Afifsuryaramadhan1 <afifsuryaramadhan@gmail.com>
2021-12-27 09:58:42 +0700

    ready

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') != 'local') {
+            URL::forceScheme('https');
+        }
+
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
--- resources/views/index1.blade.php
+++ resources/views/index1.blade.php
@@ -81,14 +81,14 @@
                         <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal0">
                             <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                 <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div><img class="img-fluid" src="{{ asset('freelancer/assets/img/portfolio/logo1.png') }}" alt="Universitas Majalengka"/>
+                            </div><img class="img-fluid" src="{{ asset('freelancer/assets/img/portfolio/logo2.png') }}" alt="Universitas Majalengka"/>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 mb-5">
                         <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
                             <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                 <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div><img class="img-fluid" src="{{ asset('freelancer/assets/img/portfolio/logo2.png') }}" alt="IAI BBC"/>
+                            </div><img class="img-fluid" src="{{ asset('freelancer/assets/img/portfolio/logo1.png') }}" alt="IAI BBC"/>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 mb-5">
